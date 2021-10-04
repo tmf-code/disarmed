3f11ae2898a337a8c514a29fb27dc5b2e7e8e884
Jae Perris <jae.perris@gmail.com>
2021-10-04 13:13:39 +0100

    Move Range to global namespace

--- Assets/Avatar/InverseKinematics/Range.cs
+++ Assets/Avatar/InverseKinematics/Range.cs
@@ -1,16 +1,11 @@
-﻿partial class Solve3D
+﻿public struct Range
 {
-  public struct Range
-  {
-    public readonly float min;
-    public readonly float max;
+  public readonly float min;
+  public readonly float max;
 
-    public Range(float min, float max)
-    {
-      this.min = min;
-      this.max = max;
-    }
+  public Range(float min, float max)
+  {
+    this.min = min;
+    this.max = max;
   }
 }
-
-
