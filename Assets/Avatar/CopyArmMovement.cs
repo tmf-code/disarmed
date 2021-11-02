a2f7e4c6a5de011d724b1c247b8cabd1344823e0
Jae Perris <jae.perris@gmail.com>
2021-11-02 10:31:24 +0100

    Fix pivot point

--- Assets/Avatar/CopyArmMovement.cs
+++ Assets/Avatar/CopyArmMovement.cs
@@ -36,8 +36,8 @@ public class CopyArmMovement : MonoBehaviour
     var shoulder = model.FindRecursiveOrThrow($"b_{handPrefix}_shoulder");
     var distance = model.transform.position - shoulder.transform.position;
 
-    model.localPosition = Vector3.Lerp(model.localPosition, distance, 1);
-    model.localRotation = Quaternion.Slerp(model.localRotation, rotation, strength);
+    model.localPosition = Vector3.Lerp(model.localPosition, Quaternion.Inverse(model.rotation) * distance, strength);
+    // model.localRotation = Quaternion.Slerp(model.localRotation, rotation, strength);
 
     // Copy IK from other arm
     var wrist = model.FindRecursiveOrThrow($"b_{handPrefix}_wrist");
@@ -48,9 +48,9 @@ public class CopyArmMovement : MonoBehaviour
     var forearmOther = targetModel.FindRecursiveOrThrow($"b_{handPrefix}_forearm_stub");
     var humerusOther = targetModel.FindRecursiveOrThrow($"b_{handPrefix}_humerus");
 
-    wrist.localRotation = wristOther.localRotation;
-    forearm.localRotation = forearmOther.localRotation;
-    humerus.localRotation = humerusOther.localRotation;
+    // wrist.localRotation = wristOther.localRotation;
+    // forearm.localRotation = forearmOther.localRotation;
+    // humerus.localRotation = humerusOther.localRotation;
 
     // Apply hand tracking
     void CopyTransform(Transform target)
