1127f9aa3a13acf9405949e0cb7b3b27481f6234
Jae Perris <jae.perris@gmail.com>
2021-11-04 18:44:38 +0100

    Improve CopyArmMovement performance

--- Assets/Scripts/CopyArmMovement.cs
+++ Assets/Scripts/CopyArmMovement.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using UnityEngine;
 
 public class CopyArmMovement : MonoBehaviour
@@ -5,12 +7,18 @@ public class CopyArmMovement : MonoBehaviour
   [Range(0, 1)]
   public float strength = 1.0F;
   public GameObject targetArm;
-  private Transform model;
+  private ChildDictionary targetChildDictionary;
+  private Tuple<Transform, Transform>[] pairedChildrenToCopyTransforms;
+  private ChildDictionary childDictionary;
   private Handedness handedness;
+  private Transform forearm;
+  private Transform humerus;
+  private Transform forearmOther;
+  private Transform humerusOther;
 
   void Start()
   {
-    model = transform.FindRecursiveOrThrow("Model");
+    childDictionary = gameObject.GetComponentOrThrow<ChildDictionary>();
     handedness = gameObject.GetComponentOrThrow<Handedness>();
 
     var playerArms = GameObject.Find("Player").GetComponentOrThrow<PlayerArms>();
@@ -19,47 +27,36 @@ public class CopyArmMovement : MonoBehaviour
       Handedness.HandTypes.HandLeft => playerArms.left,
       _ => playerArms.right,
     };
+
+    targetChildDictionary = targetArm.GetComponentOrThrow<ChildDictionary>();
+
+    pairedChildrenToCopyTransforms = targetChildDictionary.modelChildren.Values.Where(child =>
+    {
+      return BoneNameToBoneId.IsTrackedBone(child.name) && child.name != "b_l_forearm_stub" && child.name != "b_r_forearm_stub";
+    }).Select((child) => new Tuple<Transform, Transform>(child.transform, childDictionary.modelChildren.GetValue(child.name).transform)).ToArray();
+
+    var handPrefix = handedness.HandPrefix();
+
+    forearm = childDictionary.modelChildren.GetValue($"b_{handPrefix}_forearm_stub").transform;
+    humerus = childDictionary.modelChildren.GetValue($"b_{handPrefix}_humerus").transform;
+
+    forearmOther = targetChildDictionary.modelChildren.GetValue($"b_{handPrefix}_forearm_stub").transform;
+    humerusOther = targetChildDictionary.modelChildren.GetValue($"b_{handPrefix}_humerus").transform;
+
   }
 
   void Update()
   {
-    var handedness = gameObject.GetComponentOrThrow<Handedness>();
-    var targetName = handedness.IsLeft()
-      ? "ShoulderLeft"
-      : "ShoulderRight";
-
-
-    var targetModel = targetArm.transform.FindRecursiveOrThrow("Model");
-
-    var handPrefix = handedness.HandPrefix();
 
     // Copy IK from other arm
-    var forearm = model.FindRecursiveOrThrow($"b_{handPrefix}_forearm_stub");
-    var humerus = model.FindRecursiveOrThrow($"b_{handPrefix}_humerus");
-
-    var forearmOther = targetModel.FindRecursiveOrThrow($"b_{handPrefix}_forearm_stub");
-    var humerusOther = targetModel.FindRecursiveOrThrow($"b_{handPrefix}_humerus");
-
     forearm.localRotation = forearmOther.localRotation;
     humerus.localRotation = humerusOther.localRotation;
 
-    // Apply hand tracking
-    void CopyTransform(Transform target)
+    foreach (var sourceAndDestination in pairedChildrenToCopyTransforms)
     {
-      if (!BoneNameToBoneId.IsTrackedBone(target.name)) return;
-      if (target.name == "b_l_forearm_stub" || target.name == "b_r_forearm_stub")
-      {
-        // Bone shouldn't be controlled by tracking. Conflicts with IK
-        return;
-      }
-
-      var current = model.FindChildRecursive(target.name);
-      if (current == null) return;
-      current.localPosition = Vector3.Lerp(current.localPosition, target.localPosition, strength);
-      current.localRotation = Quaternion.Slerp(current.localRotation, target.localRotation, strength);
-      current.localScale = Vector3.Lerp(current.localScale, target.localScale, strength);
+      var current = sourceAndDestination.Item2;
+      var source = sourceAndDestination.Item1;
+      current.LerpLocal(source, strength);
     }
-
-    targetModel.TraverseChildren(CopyTransform);
   }
 }
