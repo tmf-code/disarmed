8a7c2255b00ffb636ef86d62a7da71b16b6b8f56
Finn Jacobsen <finn@marancon.de>
2021-11-09 17:32:07 +0100

    Better circle Animation

--- Assets/Vector-Animation-research/RadialIndicator.cs
+++ Assets/Vector-Animation-research/RadialIndicator.cs
@@ -1,45 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class RadialIndicator : MonoBehaviour
 {
     [Header("Radial Timers")]
-    [SerializeField] private float indicatorTimer = 0.0f;
-    [SerializeField] private float maxIndicatorTimer = 1.0f;
+    [SerializeField]
+    private float indicatorTimer = 0.0f;
+
+    [SerializeField]
+    private float maxIndicatorTimer = 1.0f;
 
     [Header("UI Indicator")]
-    [SerializeField] private Image radialIndicatorUI = null;
+    [SerializeField]
+    private Image radialIndicatorUI = null;
 
     [Header("Key Codes")]
-    [SerializeField] private KeyCode selectKey = KeyCode.Mouse0;
+    [SerializeField]
+    private KeyCode selectKey = KeyCode.Mouse0;
 
     [Header("Unity Event")]
-    [SerializeField] private UnityEvent myEvent = null;
+    [SerializeField]
+    private UnityEvent myEvent = null;
 
     private bool shouldUpdate = false;
 
-    private void Update(){
-        if(Input.GetKey(selectKey)){
+    private bool active = false;
+
+    private void Update()
+    {
+
+        active = Input.GetKey(selectKey);  
+
+        if (active)
+        {
             shouldUpdate = false;
             indicatorTimer += Time.deltaTime * 0.5f;
             radialIndicatorUI.enabled = true;
             radialIndicatorUI.fillAmount = indicatorTimer;
 
-            if (indicatorTimer >= maxIndicatorTimer){
-            indicatorTimer = maxIndicatorTimer;
-            radialIndicatorUI.fillAmount = maxIndicatorTimer;
-            radialIndicatorUI.enabled = false;
-            myEvent.Invoke();
+            if (indicatorTimer >= maxIndicatorTimer)
+            {
+                indicatorTimer = maxIndicatorTimer;
+                radialIndicatorUI.fillAmount = maxIndicatorTimer;
+                radialIndicatorUI.enabled = false;
+                myEvent.Invoke();
+            }
         }
-        } else {
-            if(shouldUpdate){
+        else
+        {
+            if (shouldUpdate)
+            {
                 indicatorTimer -= Time.deltaTime;
                 radialIndicatorUI.fillAmount = indicatorTimer;
 
-                if(indicatorTimer <= 0){
+                if (indicatorTimer <= 0)
+                {
                     indicatorTimer = 0;
                     radialIndicatorUI.fillAmount = 0;
                     radialIndicatorUI.enabled = false;
@@ -48,7 +66,8 @@ public class RadialIndicator : MonoBehaviour
             }
         }
 
-        if(Input.GetKeyUp(selectKey)){
+        if (Input.GetKeyUp(selectKey))
+        {
             shouldUpdate = true;
         }
     }
