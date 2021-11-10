ca32c2328f8b4712733cd69a7ce56e905cb472ba
Finn Jacobsen <finn@marancon.de>
2021-11-10 16:24:39 +0100

    added progress indicator for hand fitting in Main timeline

--- Assets/Scripts/BothHandsHoverTrigger.cs
+++ Assets/Scripts/BothHandsHoverTrigger.cs
@@ -1,18 +1,41 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BothHandsHoverTrigger : MonoBehaviour
 
 {
   public HandHoverTrigger rightHand;
   public HandHoverTrigger leftHand;
+  public Image indicator = null;
   public bool isHoverComplete = false;
+  public bool isHoveringBoth = false;
+  private float progression = 0f;
+  public float hoverDuration = 4f;
 
   void Update()
   {
-    isHoverComplete = rightHand.isHoverComplete && leftHand.isHoverComplete;
+
+
+    if (rightHand.inHoverPosition && leftHand.inHoverPosition && !isHoverComplete)
+    {
+      isHoveringBoth = true;
+      progression += Time.deltaTime;
+      rightHand.setProgression(progression / hoverDuration);
+      leftHand.setProgression(progression / hoverDuration);
+      indicator.fillAmount = progression / hoverDuration;
+    }
+    else if (!isHoverComplete)
+    {
+      isHoveringBoth = false;
+      progression = 0;
+      indicator.fillAmount = 0;
+    }
+
+    isHoverComplete = progression >= hoverDuration;
 
     if (!isHoverComplete) return;
 
+    indicator.fillAmount = 0;
     rightHand.transform.FindRecursiveOrThrow("r_handMeshNode")
       .GetComponent<MeshRenderer>().material
       .SetColor("_Color", Color.green);
