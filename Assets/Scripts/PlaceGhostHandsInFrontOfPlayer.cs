c517517aff6f22619bb1d2507e1ffa327243b2f3
Finn Jacobsen <finn@marancon.de>
2021-11-11 17:00:26 +0100

    Make arms stick to camera

--- Assets/Scripts/PlaceGhostHandsInFrontOfPlayer.cs
+++ Assets/Scripts/PlaceGhostHandsInFrontOfPlayer.cs
@@ -3,9 +3,18 @@ using UnityEngine;
 public class PlaceGhostHandsInFrontOfPlayer : MonoBehaviour
 {
 
+  public Camera headset;
+
+  public float lerpSpeed = 0.9f;
+
   void Start()
   {
-    Debug.Log("Not implemented");
+    //Debug.Log("Not implemented");
   }
 
+  void Update()
+  {
+    transform.localPosition = Vector3.Lerp(transform.localPosition, headset.transform.localPosition, 0.5f);
+    transform.localRotation = Quaternion.Slerp(transform.localRotation, headset.transform.localRotation, 0.5f);
+  }
 }
